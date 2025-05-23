f8ecb062129bc95bbfbaef7d586fe8d2f6c91b5a
Mirlan2121 <askarbekovmirlan40@gmail.com>
2025-05-23 11:40:47 +0900

    Было добавлено логгирование и тест на запрос GET 200

--- go_4.go
+++ go_4.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -22,6 +23,24 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 		r.RemoteAddr,
 		time.Since(start),
 	)
+
+	// Логирование запроса INFO
+	log.Printf("[INFO] %s %s | Client: %s | Time: %v",
+		r.Method,
+		r.URL.Path,
+		r.RemoteAddr,
+		time.Since(start))
+
+	file, _ := os.OpenFile("server.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	log.SetOutput(file)
+}
+
+func LoggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		next.ServeHTTP(w, r)
+		log.Printf("Request: %s %s | Duration: %v", r.Method, r.URL.Path, time.Since(start))
+	})
 }
 
 func main() {
