ed4634687fc319de6b028f462ef264726b64e1a6
Mirlan2121 <askarbekovmirlan40@gmail.com>
2025-05-23 10:38:17 +0900

    Было добавлено логгирование

--- go_4.go
+++ go_4.go
@@ -2,15 +2,30 @@ package main
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
+	"time"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		response := map[string]string{"status": "I AM THE STORM"}
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(response)
-	})
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+	start := time.Now() // Засекаем время
 
+	response := map[string]string{"status": "I AM THE STORM"}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+
+	// Логируем запрос
+	log.Printf(
+		"Request: %s %s | From: %s | Duration: %v",
+		r.Method,
+		r.URL.Path,
+		r.RemoteAddr,
+		time.Since(start),
+	)
+}
+
+func main() {
+	http.HandleFunc("/", handleRequest)
+	log.Println("Server started on :8080")
 	http.ListenAndServe(":8080", nil)
 }
